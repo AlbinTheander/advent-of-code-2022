9800140fc79ae3b62d476f612d401ebb64552d4e
Albin Theander <Albin.Theander@jayway.com>
2022-12-01 13:44:52 +0100

    Day 1 - typescript

--- typescript/src/days/day01.ts
+++ typescript/src/days/day01.ts
@@ -1,7 +1,12 @@
 export function day01(data: string) {
-    const result1 = data.length;
-    const result2 = data[0];
+    const elves = data.
+        split('\n\n').
+        map(elfData => elfData.split('\n').map(Number).reduce((a, b) => a + b)).
+        sort((a, b) => b - a)
 
-    console.log('Answer 1', result1);
-    console.log('Answer 2', result2);
+    const result1 = elves[0];
+    const result2 = elves[0] + elves[1] + elves[2];
+
+    console.log('The elf with the most calories is bringing', result1);
+    console.log('The top three elves are bringing', result2);
 }
