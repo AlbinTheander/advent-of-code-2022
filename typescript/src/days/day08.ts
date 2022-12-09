6b062420e7b3f34cc4fa5a33684f67edfe86d4fc
Albin Theander <Albin.Theander@jayway.com>
2022-12-09 11:45:40 +0100

    Day 8 - typescript cleanup

--- typescript/src/days/day08.ts
+++ typescript/src/days/day08.ts
@@ -1,111 +1,50 @@
+import { arrayCopy2, column, prod, sum, to2DArray } from "./utils";
+
 export function day08(data: string) {
-  const lines = data.split('\n');
-  const map = lines.map(line => Array(line.length).fill('x'));
-  const lastRow = lines.length - 1;
-  const lastCol = lines[0].length - 1;
-  for(let row = 0; row < lines.length; row++) {
-    let highest = -Infinity
-    for(let col = 0; col <= lastCol; col++) {
-      if (+lines[row][col] > highest) {
-        map[row][col] = 'v';
-        highest = +lines[row][col]
-      }
-    }
-    highest = -Infinity
-    for(let col = lastCol; col >= 0; col--) {
-      if (+lines[row][col] > highest) {
-        map[row][col] = 'v';
-        highest = +lines[row][col]
-      }
-    }
-  }
-  for(let col = 0; col < lines.length; col++) {
-    let highest = -Infinity
-    for(let row = 0; row <= lastRow; row++) {
-      if (+lines[row][col] > highest) {
-        map[row][col] = 'v';
-        highest = +lines[row][col]
-      }
-    }
-    highest = -Infinity
-    for(let row = lastRow; row >= 0; row--) {
-      if (+lines[row][col] > highest) {
-        map[row][col] = 'v';
-        highest = +lines[row][col]
-      }
-    }
-  }
+  const trees = to2DArray(data, '', true)
 
-  const count = map.flat().filter(ch => ch === 'v').length;
-  console.log(lines)
-  console.log(map)
+  const result1 = part1(trees);
+  const result2 = part2(trees);
 
-  const result1 = count;
-  const result2 = part2(lines);
-
-  console.log('First result', result1);
-  console.log('Second result', result2);
+  console.log('Number of visible trees', result1);
+  console.log('View score for the best tree house position', result2);
 }
 
-function part2(olines: string[]) {
-  const map = olines.map(line => Array(line.length).fill(1));
-  const trees = olines.map(line => [...line].map(ch => +ch))
-
-  const lastRow = trees.length - 1;
-  const lastCol = trees[0].length - 1;
-
-  for(let row = 0; row < trees.length; row++) {
-    for(let col = 0; col < trees[0].length; col++) {
-      const tree = trees[row][col];
-      let r = row-1;
-      let c = col;
-      let count = 0
-      let stop = false
-      while(!stop) {
-        if (r < 0) { stop = true; break;}
-        count++;
-        if (trees[r][col] >= tree) stop = true
-        r--;
-      }
-      map[row][col] *= count;
-
-      r = row+1;
-      c = col;
-      count = 0
-      stop = false
-      while(!stop) {
-        if (r > lastRow) { stop = true; break;}
-        count++;
-        if (trees[r][col] >= tree) stop = true
-        r++;
-      }
-      map[row][col] *= count;
-
-      r = row;
-      c = col-1;
-      count = 0
-      stop = false
-      while(!stop) {
-        if (c < 0) { stop = true; break;}
-        count++;
-        if (trees[row][c] >= tree) stop = true
-        c--;
-      }
-      map[row][col] *= count;
-
-      r = row;
-      c = col+1;
-      count = 0
-      stop = false
-      while(!stop) {
-        if (c > lastCol) { stop = true; break;}
-        count++;
-        if (trees[row][c] >= tree) stop = true
-        c++;
-      }
-      map[row][col] *= count;
-    }
+function part1(trees: number[][]): number {
+  const canBeSeen = (x: number, y: number) => {
+    const height = trees[y][x]
+    const views = treeViews(trees, x, y);
+    return views.some(direction => direction.every(t => t < height))
   }
-  console.log(map)
-  console.log(Math.max(...map.flat()))
+  let count = 0;
+  trees.forEach((row, y) => row.forEach((_, x) => {
+    if (canBeSeen(x, y)) count++;
+  }))
+  return count;
+}
+
+function part2(trees: number[][]) {
+  const viewSize = (x: number, y: number): number => {
+    const views = treeViews(trees, x, y)
+    return views.map(view => {
+      let n = 0
+      while(n < view.length && view[n] < trees[y][x]) n++;
+      if (n < view.length) n++;
+      return n;
+    }).reduce(prod)
+  }
+
+  const viewSizes = trees.flatMap((row, y) => row.map((_, x) => viewSize(x, y)))
+  return Math.max(...viewSizes)
+}
+
+const treeViews = (trees: number[][], x: number, y: number): [number[], number[], number[], number[]] => {
+  const row = trees[y]
+  const col = column(trees, x)
+  const left = row.slice(0, x).reverse()
+  const right = row.slice(x+1)
+  const above = col.slice(0, y).reverse()
+  const below = col.slice(y+1)
+
+  return [above, right, below, left];
 }
--- typescript/src/days/utils.ts
+++ typescript/src/days/utils.ts
@@ -1,1 +1,27 @@
 export const sum = (a: number, b: number): number => a + b;
+export const prod = (a: number, b: number): number => a * b;
+
+export function to2DArray(lines: string, deliminator: string, convertToNumber: true): number[][]
+export function to2DArray(lines: string, deliminator: string, convertToNumber: false): string[][]
+export function to2DArray(lines: string, deliminator = '', convertToNumber = false): string[][] | number[][] {
+  const content = lines.
+    split('\n').
+    map(line => line.split(deliminator))
+
+  if (convertToNumber) {
+    return content.map(line => line.map(item => +item))
+  }
+  return content
+}
+
+export function arrayCopy2<Item>(data: Item[][], fillWith: undefined): Item[][]
+export function arrayCopy2<Item>(data: unknown[][], fillWith: Item): Item[][]
+export function arrayCopy2(data, fillWith = undefined) {
+  const newData = data.map(row => row.map(item => fillWith === undefined ? item : fillWith));
+  return newData
+}
+
+export const column = <B>(array2D: B[][], col: number): B[] => {
+  return array2D.map(row => row[col]);
+}
+  
