1078b366d62284d216091a0474c27cb2446c3181
Albin Theander <Albin.Theander@jayway.com>
2022-12-09 15:40:38 +0100

    Day 9 - typescript cleanup

--- typescript/src/days/day09.ts
+++ typescript/src/days/day09.ts
@@ -1,66 +1,68 @@
-type Move = [string, number];
+type Move = [string, number]
+type Pos = { x: number, y: number }
+type Rope = Pos[]
 
 export function day09(data: string) {
   const moves: Move[] = data.split('\n').map(line => {
     const parts = line.split(' ');
     return [parts[0], +parts[1]];
   })
-  const result1 = part2(moves);
-  const result2 = data[1];
+  const result1 = part1(moves);
+  const result2 = part2(moves);
 
-  console.log('First result', result1);
-  console.log('Second result', result2);
+  console.log('The tail of the two knot rope covers', result1, 'coordinates');
+  console.log('The tail of the ten knot rope covers', result2, 'coordinates');
 }
 
 function part1(moves: Move[]) {
-  let headX = 0;
-  let headY = 0;
-  let tailX = 0;
-  let tailY = 0;
-  let tailsPos = new Set<string>();
-  for (let [dir, length] of moves) {
-    console.log('Moving', dir, length);
-    for(let i = 0; i < length; i++) {
-      switch(dir) {
-        case 'U': headY--; break;
-        case 'R': headX++; break;
-        case 'D': headY++; break;
-        case 'L': headX--; break;
-      }
-      ([tailX, tailY] = moveNext([headX, headY], [tailX, tailY]))
-      tailsPos.add(tailX + '-' + tailY);
-    }
-  }
-  console.log(tailsPos);
-  console.log(tailsPos.size)
+  const rope = Array(2).fill({x: 0, y: 0});
+  return runIt(rope, moves);
 }
+
 function part2(moves: Move[]) {
-  let chain: [number, number][] = Array(10).fill(0).map(() => [0, 0]);
-  let tailsPos = new Set<string>();
-  for (let [dir, length] of moves) {
-
-    for(let i = 0; i < length; i++) {
-      switch(dir) {
-        case 'U': chain[0][1]--; break;
-        case 'R': chain[0][0]++; break;
-        case 'D': chain[0][1]++; break;
-        case 'L': chain[0][0]--; break;
-      }
-      for (let p = 1; p < chain.length; p++ ) {
-        (chain[p] = moveNext(chain[p-1], chain[p]))
-      }
-      tailsPos.add(chain[9].toString());
-    }
-  }
-  console.log(tailsPos);
-  console.log(tailsPos.size)
+  const rope = Array(10).fill({x: 0, y: 0});
+  return runIt(rope, moves);
 }
 
-function moveNext([x1, y1], [x2, y2]): [number, number] {
-  let x3 = x2, y3 = y2
-  if (Math.abs(x1 - x2) > 1 || Math.abs(y1 - y2) > 1) {
-    if (x2 < x1) { x3++ } else if (x2 > x1) { x3-- }
-    if (y2 < y1) { y3++ } else if (y2 > y1) { y3-- }
+function runIt(originalRope: Rope, moves: Move[]): number {
+  let rope = originalRope;
+  let tailPositions = new Set<string>();
+
+  for(let [direction, length] of moves) {
+    for(let i = 0; i < length; i++) {
+      rope = update(rope, direction);
+      const {x, y} = rope[rope.length-1];
+      tailPositions.add(`${x},${y}`)
+    }
   }
-  return [x3, y3];
+  return tailPositions.size;
+}
+
+function update(rope: Rope, direction: string): Rope {
+  const newRope = [];
+  newRope.push(movePos(rope[0], direction));
+  for (let i = 1; i < rope.length; i++) {
+    newRope.push(moveNext(newRope[i-1], rope[i]));
+  }
+  return newRope;
+}
+
+function movePos({x, y}: Pos, direction: string): Pos {
+  switch(direction) {
+    case 'U': return { x, y: y-1 };
+    case 'R': return { x: x+1, y };
+    case 'D': return { x, y: y+1 };
+    case 'L': return { x: x-1, y };
+  }
+}
+
+function moveNext(target: Pos, pos: Pos): Pos {
+  if (Math.abs(target.x - pos.x) > 1 || Math.abs(target.y - pos.y) > 1) {
+    const x = (pos.x < target.x) ? (pos.x + 1) :
+              (pos.x > target.x) ? (pos.x - 1) : pos.x
+    const y = (pos.y < target.y) ? (pos.y + 1) :
+              (pos.y > target.y) ? (pos.y - 1) : pos.y
+    return {x, y}
+  }
+  return pos
 }
