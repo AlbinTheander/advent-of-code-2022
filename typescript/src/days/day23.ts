bac9c8fc706794886cf8416d6982df1665f17e87
Albin Theander <Albin.Theander@jayway.com>
2022-12-27 22:53:04 +0100

    Cleanup day 23

--- typescript/src/days/day23.ts
+++ typescript/src/days/day23.ts
@@ -2,11 +2,10 @@ import { Infinite2DSet } from "./utils/infinite2DSet";
 
 export function day23(data: string) {
   const map = parse(data);
-  const result1 = part1(map);
-  const result2 = data[1];
+  const [result1, result2] = part1and2(map);
 
-  console.log("First result", result1);
-  console.log("Second result", result2);
+  console.log("The number of empty squares after 10 rounds are", result1);
+  console.log("The elves stop moving after", result2, 'rounds');
 }
 
 function parse(data: string): Infinite2DSet {
@@ -20,15 +19,17 @@ function parse(data: string): Infinite2DSet {
   return map;
 }
 
-function part1(map: Infinite2DSet) {
+function part1and2(map: Infinite2DSet): [number, number] {
   const directions = [[0, -1], [0, 1], [-1, 0], [1, 0]]
   const rotateDirections = () => directions.push(directions.shift())
+
   const alone = (x, y) => {
     for(let dx = -1; dx <= 1; dx++)
       for(let dy = -1; dy <= 1; dy++)
         if ((dx !== 0 || dy !== 0) && map.has(x+dx, y+dy)) return false
     return true
   }
+  
   const getMove = (x, y) => {
     for(let [dx, dy] of directions) {
       if (dy === 0) {
@@ -40,9 +41,16 @@ function part1(map: Infinite2DSet) {
     return null
   }
 
+  const getSizeScore = () => {
+    const [minX, minY, maxX, maxY] = map.bounds
+    return (maxX - minX + 1)*(maxY-minY+1) - map.size
+  }
 
-  let steps = 0
-  while(steps++ > -1) {
+
+  let rounds = 0
+  let after10 = 0
+  while(true) {
+    if (rounds === 10) after10 = getSizeScore()
     const moves = []
     const once = new Set<string>()
     const twice = new Set<string>()
@@ -67,11 +75,9 @@ function part1(map: Infinite2DSet) {
         map.add(x1, y1)
       }
     })
+    rounds++
     if (moves.length === 0) break;
     rotateDirections()
   }
-  let [minX, minY, maxX, maxY] = map.bounds
-  const size = (maxX - minX + 1)*(maxY-minY+1)
-  console.log(size, map.size, size-map.size)
-  console.log('Rounds', steps)
+  return [after10, rounds]
 }
